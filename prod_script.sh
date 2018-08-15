712b7c94c8189c47db24607c05f6a0f55c4f920e
Yogesh Jadhav <yogse7en@gmail.com>
2018-08-15 17:26:02 +0530

    Fix script

--- prod_script.sh
+++ prod_script.sh
@@ -1,4 +1,4 @@
-cd /home/yogesh/apps/play-apps/restful-api-app/
+cd /home/yogesh/apps/play-apps/restful-api-service-backend/
 lsof -n -i :9000 | grep LISTEN | awk '{ print $2 }' | uniq | xargs kill -9
 mkdir -p target/RUNNING_PID/
 rm -rf target/RUNNING_PID/play.pid
